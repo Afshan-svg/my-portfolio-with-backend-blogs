d3354444d4e31f702372ccc9cd38d45f27c29e9d
Afshan Khan <afshan.ayubkhan@gmail.com>
2025-12-02 20:56:51 +0530

    cors issue fixed

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import { constructMessages } from "./prompt.js";
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:5173', "https://afshankhandasankop.netlify.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
